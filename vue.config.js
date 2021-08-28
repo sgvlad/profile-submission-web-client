f54f65e6463b9452d7179959b56f4fd661d9d7a1
stoianv <st0ian.vlad.george@gmail.com>
2021-08-28 18:49:14 +0300

    Added localhost as proxy

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "^/": {
-        target: "https://profile-submission2021.herokuapp.com",
-        // target: "http://localhost:8080",
+        target: "http://localhost:8080",
         changeOrigin: true
       }
     }
